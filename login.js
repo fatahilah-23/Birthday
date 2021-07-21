376cc153e405ba2196cf59ec0896ddf9ea4f3e61
Jangki Fatahilah <82248666+fatahilah-23@users.noreply.github.com>
2021-07-21 16:25:17 +0700

    add

---
+++ login.js
@@ -0,0 +1,12 @@
+var a=prompt("Masukin Nama Sayang");
+ var b=prompt("Masukin Tanggal Lahir Sayang");
+  	if(a== "Ririn Anjasari" && b=="02-08-2001")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
