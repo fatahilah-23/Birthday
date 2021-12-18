0f529c24f710ec408aeb966afaaac0d1e009cf9f
Jangki Fatahilah <82248666+fatahilah-23@users.noreply.github.com>
2021-12-18 18:44:51 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 var a=prompt("Masukin Nama Anda sesuai KTP");
  var b=prompt("Masukin Tanggal Lahir dengan format dd/mm/yyyy");
-  	if(a== "Diah Ayu Anggraeni" && b=="26-08-2002")
+  	if(a== "Syifa Syahru Ramadani" && b=="18/12/2000")
   	{
   	    window.location.href="start.html";
   	}
